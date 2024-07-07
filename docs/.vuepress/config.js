8c92c5e2532e7b14456d4fbe48b0ff76832b56d4
方漓猫 <mucigames@outlook.com>
2024-07-07 16:36:32 +0800

    add version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,12 @@ import { plumeTheme } from 'vuepress-theme-plume'
 export default defineUserConfig({
   lang: 'zh-CN',
   theme: plumeTheme({
-    // more...
+    profile: {
+      name: '隐藏之年开发组',
+      description: '星屹工作室下隐藏之年的专门开发组',
+      avatar: '/team.png',
+      circle: false, 
+    }
   }),
   bundler: viteBundler(),
 })
