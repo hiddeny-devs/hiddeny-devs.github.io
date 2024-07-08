0e94cd0c75a0a0a493e89232ec1a2146a3c69a7e
方漓猫 <mucigames@outlook.com>
2024-07-08 08:14:45 +0800

    Revert "fix"
    This reverts commit b2941baebbec91584f1e2c4cab78b3d7565caa9d.

---
+++ docs/.vuepress/navbar.js
@@ -0,0 +1,9 @@
+import { defineNavbar } from 'vuepress-theme-plume'
+
+export default defineNavbar([
+  { text: '首页', link: '/', icon: 'material-symbols:home-outline' },
+  { text: '博客', link: '/blog/', icon: 'material-symbols:article-outline' },
+  { text: '教程', link: '/docs/tutorials/index' },
+  { text: 'API', link: '/docs/api/README' },
+  { text: '关于', link: '/docs/about/credits'}
+])
