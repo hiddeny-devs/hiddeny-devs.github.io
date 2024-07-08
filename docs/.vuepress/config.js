d859a9a89383ddfb35bcfda0b4f77049a13290ee
方漓猫 <mucigames@outlook.com>
2024-07-08 08:05:48 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,13 +14,6 @@ export default defineUserConfig({
       organization: '星屹工作室',
       location: '地球'
     },
-    navbar: [
-      { text: '首页', link: '/', icon: 'material-symbols:home-outline' },
-      { text: '博客', link: '/blog/', icon: 'material-symbols:article-outline' },
-      { text: '教程', link: '/docs/tutorials/index' },
-      { text: 'API', link: '/docs/api/README' },
-      { text: '关于', link: '/docs/about/credits'}
-    ],
      notes: {
       dir: '/docs/', // 声明所有笔记的目录
       link: '/', // 声明所有笔记默认的链接前缀， 默认为 '/'
